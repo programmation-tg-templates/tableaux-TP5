eff7b0f68ffccaf3ba44c95f74f8292321b81335
François Roland <francois.roland@heh.be>
2025-10-11 22:12:04 +0200

    Refactor test cases for 'inverser' function to use 'test' instead of 'it' for consistency

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -3,23 +3,23 @@
 import { inverser } from "../index";
 
 describe("inverser", () => {
-  it("devrait inverser un tableau de plusieurs éléments", () => {
+  test("devrait inverser un tableau de plusieurs éléments", () => {
     expect(inverser([1, 2, 3, 4])).toEqual([4, 3, 2, 1]);
   });
 
-  it("devrait inverser un tableau vide", () => {
+  test("devrait inverser un tableau vide", () => {
     expect(inverser([])).toEqual([]);
   });
 
-  it("devrait inverser un tableau avec un seul élément", () => {
+  test("devrait inverser un tableau avec un seul élément", () => {
     expect(inverser([42])).toEqual([42]);
   });
 
-  it("devrait inverser un tableau avec des éléments négatifs", () => {
+  test("devrait inverser un tableau avec des éléments négatifs", () => {
     expect(inverser([-1, -2, -3])).toEqual([-3, -2, -1]);
   });
 
-  it("devrait inverser un tableau avec des éléments identiques", () => {
+  test("devrait inverser un tableau avec des éléments identiques", () => {
     expect(inverser([5, 5, 5])).toEqual([5, 5, 5]);
   });
 });
